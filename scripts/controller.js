da12e4f8c4778ca87c559936e39d8655e20df6e9
Hiten Vats <hitenvats16@gmail.com>
2022-08-03 22:16:52 +0530

    Add files via upload

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,15 +1,17 @@
-function compute(){
+import {salaryOperations, netSalary} from './model.js'
+
+function compute() {
     console.log('inside compute')
     let basicSalary = document.querySelector('#salaryInput').value
-    let paras = document.getElementsByTagName('p')
-    paras[0].innerText = salaryOperations.da(basicSalary)
-    paras[1].innerText = salaryOperations.hra(basicSalary)
-    paras[2].innerText = salaryOperations.ta(basicSalary)
+    for(let key in salaryOperations){
+        document.getElementById(key).innerText = salaryOperations[key](basicSalary)
+    }
+    document.querySelector('#netBox').innerText = netSalary(basicSalary)
 }
 
-function bindEvents(){
+function bindEvents() {
     let btn = document.querySelector('#calculateButton')
-    btn.addEventListener("click",compute) // DOUBT : using compute() instead of compute gets called automaticaly without btn being clicked....  
+    btn.addEventListener("click", compute)
 }
 
-window.addEventListener("load",bindEvents())
+window.addEventListener("load", bindEvents())
--- scripts/model.js
+++ scripts/model.js
@@ -1,14 +1,29 @@
-var salaryOperations = {
+export const salaryOperations = {
+    basicSalary:0,
     da(basicSalary){
         console.log('inside da')
-        return 0.2 * basicSalary
+        return 0.2 * this.basicSalary
     },
     hra(basicSalary){
         console.log('inside hra')
-        return 0.5 * basicSalary
+        return 0.5 * this.basicSalary
     },
     ta(basicSalary){
         console.log('inside ta')
-        return 0.3 * basicSalary
+        return 0.3 * this.basicSalary
+    },
+    pf(basicSalary){
+        console.log('inside pf')
+        return 0.05 * this.basicSalary
     }
 }
+
+export function takeSalary(basicSalary){
+    salaryOperations.basicSalary = basicSalary
+}
+
+export function netSalary(basicSalary){
+    console.log('inside netSalary')
+    return (salaryOperations.da(basicSalary)+salaryOperations.hra(basicSalary)+salaryOperations.ta(basicSalary))-salaryOperations.pf(basicSalary)+parseInt(basicSalary)
+}
+
